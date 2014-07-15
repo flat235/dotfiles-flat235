5643f2f39a2e0c19a7c223dd7721df5bad9f3964
Moritz Bellach <github@moritzbellach.de>
2014-07-16 01:43:47 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+$HOME/.homesick/repos/homeshick/bin/homeshick clone flat235/dotfiles-flat235
+$HOME/bin/updatehomeshick.sh
