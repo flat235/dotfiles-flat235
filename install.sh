4d6e3456cf3b378c7e9db2cbe3787631682a560c
Moritz Bellach <github@moritzbellach.de>
2015-08-10 16:35:55 +0200

    fix permission of mr in install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p ~/bin
 curl -L https://raw.githubusercontent.com/joeyh/myrepos/master/mr -o ~/bin/mr
+chmod 755 ~/bin/mr
 ~/bin/mr --trust-all bootstrap https://raw.githubusercontent.com/flat235/dotfiles-flat235/master/home/.mrconfig
