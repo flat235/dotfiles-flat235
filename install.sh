51a8469c2bce050f5d767277dbf2a9b27180200c
Moritz Bellach <moritzbellach@gmail.com>
2016-08-22 17:07:39 +0200

    clean up install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,3 @@ mkdir -p ~/bin
 curl -L https://raw.githubusercontent.com/joeyh/myrepos/master/mr -o ~/bin/mr
 chmod 755 ~/bin/mr
 ~/bin/mr --trust-all bootstrap https://raw.githubusercontent.com/flat235/dotfiles-flat235/master/home/.mrconfig
-# should be done by mr
-#~/.homesick/repos/homeshick/bin/homeshick link
