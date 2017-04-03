01acedbb54b25c5371d2980a585f0a90a65801a5
Moritz Bellach <moritzbellach@gmail.com>
2017-04-03 03:12:37 +0200

    fix homeshick to link in batch mode; long command switches

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p ~/.homesick/repos
+mkdir --parents ~/.homesick/repos
 git clone 'https://github.com/andsens/homeshick.git' ~/.homesick/repos/homeshick
 ~/.homesick/repos/homeshick/bin/homeshick clone flat235/dotfiles-flat235
-~/.homesick/repos/homeshick/bin/homeshick -f link dotfiles-flat235
+~/.homesick/repos/homeshick/bin/homeshick --force --batch link dotfiles-flat235
