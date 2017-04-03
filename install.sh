fc97024865f7f3a7045461b6ea9824ee3d936e6b
Moritz Bellach <moritzbellach@gmail.com>
2017-04-03 03:09:06 +0200

    fix homeshick repo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.homesick/repos
 git clone 'https://github.com/andsens/homeshick.git' ~/.homesick/repos/homeshick
-~/.homesick/repos/homeshick/bin/homeshick clone dotfiles-flat235
+~/.homesick/repos/homeshick/bin/homeshick clone flat235/dotfiles-flat235
 ~/.homesick/repos/homeshick/bin/homeshick -f link dotfiles-flat235
