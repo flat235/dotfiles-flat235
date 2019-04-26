fe2425a8948894e4a2f77a308ea96936c4c49598
Moritz Bellach <moritzbellach@gmail.com>
2019-04-26 15:43:52 +0200

    fish: add package manager, spavefish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,2 +1,18 @@
 source $HOME/.homesick/repos/homeshick/homeshick.fish
-set PATH $HOME/bin $HOME/.cargo/bin $PATH
+
+# PATH
+set bindirs $HOME/bin $HOME/.cargo/bin
+for dir in $bindirs do
+	if test -d $dir
+		set PATH $dir $PATH
+	end
+end
+
+set EDITOR vim
+
+# package manager
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
