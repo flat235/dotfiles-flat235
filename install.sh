1bd33d942477610717e5a204707d02c04dc75c37
Moritz Bellach <moritzbellach@gmail.com>
2019-09-09 12:34:45 +0200

    fix: remove spacevim from install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,3 @@ mkdir --parents ~/.homesick/repos
 git clone 'https://github.com/andsens/homeshick.git' ~/.homesick/repos/homeshick
 ~/.homesick/repos/homeshick/bin/homeshick clone flat235/dotfiles-flat235
 ~/.homesick/repos/homeshick/bin/homeshick --force --batch link dotfiles-flat235
-curl -sLf https://spacevim.org/install.sh | bash
