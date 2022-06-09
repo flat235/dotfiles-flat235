040b92559b0e0f2132eb7df4a74839c31524f83f
Moritz Bellach <moritzbellach@gmail.com>
2022-06-09 18:00:24 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir --parents ~/.homesick/repos
 git clone 'https://github.com/andsens/homeshick.git' ~/.homesick/repos/homeshick
-~/.homesick/repos/homeshick/bin/homeshick clone flat235/dotfiles-flat235
-~/.homesick/repos/homeshick/bin/homeshick --force --batch link dotfiles-flat235
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+echo "y" | ~/.homesick/repos/homeshick/bin/homeshick clone flat235/dotfiles-flat235
